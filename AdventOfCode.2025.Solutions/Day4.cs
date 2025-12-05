f467f8275b42da21ad96e030ed186e6bba3e0d60
Jerrett Davis <jd@jerrettdavis.com>
2025-12-04 22:57:25 -0600

    feat(day4): completed day 4 solution

---
+++ AdventOfCode.2025.Solutions/Day4.cs
@@ -0,0 +1,74 @@
+﻿using AdventOfCode.Abstractions;
+using AdventOfCode.Application;
+
+namespace AdventOfCode._2025.Solutions;
+
+public class Day4(
+    IDataFetcher fetcher,
+    ISolutionConfigurator configurator
+) : BaseSolution(configurator, fetcher)
+{
+    public override int Year => 2025;
+    public override int Day => 4;
+    
+    private string[][] Grid 
+        => Data.Split("\n", StringSplitOptions.TrimEntries)
+        .Select(s => s.Trim())
+        .Where(s => !string.IsNullOrWhiteSpace(s))
+        .Select(line => line.ToCharArray().Select(c => c.ToString()).ToArray())
+        .ToArray();
+
+    private bool AdjacentCellsHaveLessThan4Tps(string[][] g, int x, int y)
+    {
+        if (!IsTp(g[y][x])) return false;
+
+        var count = 0;
+        if (x > 0) count += IsTp(g[y][x - 1]) ? 1 : 0;
+        if (x < g[y].Length - 1) count += IsTp(g[y][x + 1]) ? 1 : 0;
+        if (y > 0) count += IsTp(g[y - 1][x]) ? 1 : 0;
+        if (y < g.Length - 1) count += IsTp(g[y + 1][x]) ? 1 : 0;
+        if (x > 0 && y > 0) count += IsTp(g[y - 1][x - 1]) ? 1 : 0;
+        if (x < g[y].Length - 1 && y > 0) count += IsTp(g[y - 1][x + 1]) ? 1 : 0;
+        if (x > 0 && y < g.Length - 1) count += IsTp(g[y + 1][x - 1]) ? 1 : 0;
+        if (x < g[y].Length - 1 && y < g.Length - 1) count += IsTp(g[y + 1][x + 1]) ? 1 : 0;
+        return count < 4;
+    }
+
+    private bool IsTp(string cell) => cell.Equals("@");
+    
+    public override long SolveA() =>
+        Grid
+            .SelectMany((row, y) => row.Select((cell, x) => (cell, x, y)))
+            .Where(c => AdjacentCellsHaveLessThan4Tps(Grid, c.x, c.y))
+            .ToArray()
+            .Length;
+
+    public override long SolveB() => CountMarked(MarkAccessible(Grid));
+
+    private string[][] MarkAccessible(string[][] grid)
+    {
+        var clone = grid.ToArray();
+        int marked, markedBefore;
+        do
+        {
+            markedBefore = CountMarked(clone);
+            foreach (var (x, y) in grid.SelectMany((row, y) => row.Select((cell, x) => (x, y)))) 
+                clone = MarkAccessible(clone, x, y);
+            
+            marked = CountMarked(clone);
+        } while (marked != markedBefore);
+
+        return clone;
+    }
+    
+    private static int CountMarked(string[][] grid) 
+        => grid.SelectMany(row => row).Count(c => c == "x");
+
+    private string[][] MarkAccessible(string[][] grid, int x, int y)
+    {
+        var clone = grid.ToArray();
+        if (!AdjacentCellsHaveLessThan4Tps(grid, x, y)) return clone;
+        clone[y][x] = "x";
+        return clone;
+    }
+}
---
+++ AdventOfCode.Tests/2025/Day4Tests.cs
@@ -0,0 +1,68 @@
+﻿using AdventOfCode._2025.Solutions;
+using AdventOfCode.Application;
+
+namespace AdventOfCode.Tests._2025;
+
+public class Day4Tests : TestBase
+{
+    public override int Year => 2025;
+    public override int Day => 4;
+
+    public override bool SkipGetData => true;
+
+    [Fact]
+    public async Task Part1_SmokeTest()
+    {
+        // Arrange
+        const string input = """
+                             ..@@.@@@@.
+                             @@@.@.@.@@
+                             @@@@@.@.@@
+                             @.@@@@..@.
+                             @@.@@@@.@@
+                             .@@@@@@@.@
+                             .@.@.@.@@@
+                             @.@@@.@@@@
+                             .@@@@@@@@.
+                             @.@.@@@.@.
+                             """;
+        
+        var inputFetcher = new TestDataFetcher(input);
+        var solver = new Day4(inputFetcher, new SolutionContext());
+        await solver.InitializeAsync();
+        
+        // Act
+        var result = solver.SolveA();
+        
+        // Assert
+        Assert.Equal(13, result);
+    }
+    
+    [Fact]
+    public async Task Part2_SmokeTest()
+    {
+        // Arrange
+        const string input = """
+                             ..@@.@@@@.
+                             @@@.@.@.@@
+                             @@@@@.@.@@
+                             @.@@@@..@.
+                             @@.@@@@.@@
+                             .@@@@@@@.@
+                             .@.@.@.@@@
+                             @.@@@.@@@@
+                             .@@@@@@@@.
+                             @.@.@@@.@.
+                             """;
+        
+        var inputFetcher = new TestDataFetcher(input);
+        var solver = new Day4(inputFetcher, new SolutionContext());
+        await solver.InitializeAsync();
+        
+        // Act
+        var result = solver.SolveB();
+        
+        // Assert
+        Assert.Equal(43, result);
+    }
+}
